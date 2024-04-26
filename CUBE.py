0443f5af1e7f620fe678936a15ec5a4067e6a946
Taiwo122 <93882023+Taiwo122@users.noreply.github.com>
2024-04-26 18:11:00 -0500

    Add files via upload

---
+++ CUBE.py
@@ -0,0 +1,30 @@
+Python 3.12.3 (v3.12.3:f6650f9ad7, Apr  9 2024, 08:18:47) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> import matplotlib.pyplot as plt
+... import numpy as np
+... 
+... # First five cubic numbers
+... x = np.arange(1, 6)
+... y = x ** 3
+... 
+... plt.figure(figsize=(10, 5))
+... 
+... plt.subplot(1, 2, 1)
+... plt.bar(x, y, color='blue')
+... plt.title('First Five Cubic Numbers')
+... plt.xlabel('Number')
+... plt.ylabel('Cube')
+... 
+... # First 5000 cubic numbers
+... x_5000 = np.arange(1, 5001)
+... y_5000 = x_5000 ** 3
+... 
+... plt.subplot(1, 2, 2)
+... plt.scatter(x_5000, y_5000, c=y_5000, cmap='viridis')
+... plt.colorbar(label='Cube')
+... plt.title('First 5000 Cubic Numbers')
+... plt.xlabel('Number')
+... plt.ylabel('Cube')
+... 
+... plt.tight_layout()
+... plt.show()
